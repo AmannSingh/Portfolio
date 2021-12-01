d27f1abc8bf1cac50f52fd023c641c1b1cc9ba10
amann singh <amann.singh7@outlook.com>
2021-11-30 18:26:01 -0800

    Update meta data

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,20 @@
 import Theme from '../styles/theme';
-import Meta from '..meta';
+import Helmet from 'react-helmet';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-    <Meta>
+    <Helmet>
+      <meta property="og:type" content="website"/>
+      <meta property="og:url" content="https://amannsingh.netlify.app/"/>
+      <meta property="og:title" content="Amann Singh "/>
+      <meta property="og:description" content="Portfolio showcasing my work as software engineer student "/>
+      <meta property="og:image" content="/images/img2.png"/>
+    </Helmet>
       <Theme>
         <Component {...pageProps} />
       </Theme>
-    </Meta>
+    
     </>
   );
 }
